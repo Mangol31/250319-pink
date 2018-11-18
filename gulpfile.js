6697e64e78282d6ab1a0a96e55432742f89dcb0e
Mangol31 <tritemy@yandex.ru>
2018-11-18 22:26:57 +0500

    Проблема с gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -15,7 +18,11 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream())
+    .pipe(gulp.dest("build/css"));
 });
 
 gulp.task("server", function () {
@@ -27,8 +34,24 @@ gulp.task("server", function () {
     ui: false
   });
 
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3})
+      ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
+// gulp.task("build", function () {
+//   return gulp.src("source/css/style.min.css")
+//     .pipe(plumber())
+//     .pipe(gulp.dest("build/css"))
+// })
+
 gulp.task("start", gulp.series("css", "server"));
+
